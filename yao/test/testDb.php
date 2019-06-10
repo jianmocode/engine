4426df03887dc53b8e6ea54b7f6605e65db37e64
Max <max@diancloud.com>
2019-06-10 19:14:35 +0800

    DB升级到  5.8

--- yao/test/testDb.php
+++ yao/test/testDb.php
@@ -14,20 +14,61 @@ DB::connect();
  * @link https://www.vpin.biz
  */
 class testDB extends TestCase {
+
+    function testSchema() {
+
+        $this->assertEquals( true, true );
+    }
+
+
+
+    /**
+     * 测试插入数据
+     */
+    function testInsert() {
+
+        DB::table('category')->truncate();
+        DB::table("category")->insert([
+            ['category_sn' => 'unit-test-1234567', "name"=>"测试1", 'icon' => json_encode(["url"=>"http://www.baidu.com", "name"=>"地址一"]) ],
+            ['category_sn' => 'unit-test-1234568', "name"=>"测试2", 'icon' => json_encode(["url"=>"http://www.baidu.com", "name"=>"地址二"]) ]
+        ]);
+
+        $id = DB::table("category")->insertGetId([
+            'category_sn' => 'unit-test-1234569', "name"=>"测试3", 'icon' => json_encode(["url"=>"http://www.baidu.com", "name"=>"地址三"]) 
+        ]);
+
+        $this->assertEquals( $id, 3 );
+    }
+
+
+    function testUpdate() {
+
+        DB::table("category")
+            ->where('category_sn', "unit-test-1234568")
+            ->update([
+                "icon->name"=> "变更名称"
+            ]);
+        $this->assertEquals( true, true );
+
+    }
     
     /**
      * 测试 accesslog
      */
     function testSelect() {
-        $rows = DB::table("store")
-                      ->select("name")
-                      ->selectRaw("COUNT(id_store) as cnt")
+
+        $count = DB::table("category")->count("id_category");
+        $this->assertEquals( $count, 3);
+
+        $rows = DB::table("category")
+                      ->where("icon->name", "=", "变更名称")
+                      ->select("name","category_sn", "icon")
+                      ->selectRaw("COUNT(id_category) as cnt")
                       ->get()
                       ->toArray()
         ;
-
         $row = current( $rows );
-        $this->assertEquals( $row["cnt"], 0 );
+        $this->assertEquals( $row["category_sn"], "unit-test-1234568" );
     }
 
 }
