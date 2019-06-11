617845433583352022b51bd52049b614679738bd
Max <max@diancloud.com>
2019-06-11 21:48:44 +0800

    迁移模型设定

--- yao/src/Model.php
+++ yao/src/Model.php
@@ -11,6 +11,11 @@
 
 namespace Yao;
 use \Illuminate\Database\Eloquent\Model as EloquentModel;
+use \League\Flysystem\AdapterInterface;
+
+defined("YAO_PUBLIC_URL") ?: define("YAO_PUBLIC_URL", $GLOBALS["YAO"]["storage"]["public"]);
+defined("YAO_PRIVATE_URL") ?: define("YAO_PRIVATE_URL", $GLOBALS["YAO"]["storage"]["private"]);
+
 
 // 连接数据库
 DB::connect();
@@ -19,4 +24,131 @@ DB::connect();
  * 数据模型 
  * see https://laravel.com/docs/5.8/eloquent
  */
-class Model extends EloquentModel {}
+class Model extends EloquentModel {
+
+    /**
+     * 公开文件访问路径
+     */
+    public $publicURL = YAO_PUBLIC_URL;
+
+
+    /**
+     * 私密的访问路径
+     */
+    public $privateURL = YAO_PRIVATE_URL;
+
+
+    /**
+     * 公开文件字段
+     */
+    public $publicFiles = [];
+    
+
+    /**
+     * 私有文件字段
+     */
+    public $privateFiles = [];
+
+
+    /**
+     * 文件前缀
+     */
+    public $filePrefix = "";
+
+    /**
+     * 处理输入文件类字段
+     */
+    public function filesInput() {
+
+        // 文件前缀
+        $filePrefix = $this->filePrefix;
+        if ( strpos( $filePrefix, "@") === 0 ) {
+            $prefix = substr($filePrefix, 1, strlen($filePrefix));
+            $filePrefix = "{$this->$prefix}/";
+        }
+
+        // 读取文件字段
+        $files = $this->getFiles();
+        foreach( $files as $attr=>$isPrivate ) {
+
+            // 单一文件路径
+            if ( is_string($this->$attr) ) {
+                $this->$attr = $this->writeFile( $this->$attr, $isPrivate, $filePrefix );
+            
+            // 二维数组
+            } else {
+
+                // 批量处理文件
+                $values = $this->$attr;
+                foreach( $values as $key=>$path ) {
+                    $values[$key] = $this->writeFile( $path, $isPrivate,  $filePrefix );
+                }
+                $this->$attr = $values;
+            }
+        }
+    }
+
+    /**
+     * 读取文件字段清单
+     */
+    private function getFiles() {
+
+        $files = [];
+        if ( !empty($this->publicFiles) ) {
+            $files = array_merge(
+                $files,
+                array_combine( $this->publicFiles, array_fill(0, count($this->publicFiles), false) )
+            );
+        }
+
+        if ( !empty($this->privateFiles) ) {
+            $files = array_merge(
+                $files,
+                array_combine( $this->privateFiles, array_fill(0, count($this->privateFiles), true) )
+            );
+        }
+
+        return $files;
+    }
+
+
+    /**
+     * 保存文件
+     * @param $path 文件路径
+     * @param $private 是否为私有文件
+     * @param $perfix 文件前缀
+     * @return string 文件名称;
+     */
+    private function writeFile( $path, $private=false, $perfix="" ) {
+
+        if ( strpos( $path, "@") !== 0 ) {
+            return $path;
+        }
+
+        $path = substr($path, 1, strlen($path));
+        if ( !file_exists($path) ) {
+            throw Excp::create("文件不存在({$path})", 404);
+        }
+
+        // 转存到指定的存储引擎
+        $ext = pathinfo($path, PATHINFO_EXTENSION);         
+        $name = FS::getPathName( $ext, $perfix );
+        $stream = fopen($path, 'r');
+        FS::writeStream($name, $stream, [
+            'visibility' => $private ? AdapterInterface::VISIBILITY_PRIVATE : AdapterInterface::VISIBILITY_PUBLIC
+        ]);
+        if (is_resource($stream)) {
+            fclose($stream);
+        }
+
+        return $name;
+    }
+
+    /**
+     * 处理输入文件类字段读取
+     */
+    public function filesOutput() {
+    }
+
+
+}
--- yao/src/Observer.php
+++ yao/src/Observer.php
@@ -10,138 +10,11 @@
  */
 
 namespace Yao;
-use \League\Flysystem\AdapterInterface;
-
-defined("YAO_PUBLIC_URL") ?: define("YAO_PUBLIC_URL", $GLOBALS["YAO"]["storage"]["public"]);
-defined("YAO_PRIVATE_URL") ?: define("YAO_PRIVATE_URL", $GLOBALS["YAO"]["storage"]["private"]);
 
 /**
  * 数据模型监听器
  */
 class Observer {
-
-    /**
-     * 公开文件访问路径
-     */
-    protected $publicURL = YAO_PUBLIC_URL;
-
-
-    /**
-     * 私密的访问路径
-     */
-    protected $privateURL = YAO_PRIVATE_URL;
-
-
-    /**
-     * 公开文件字段
-     */
-    protected $publicFiles = [];
-    
-
-    /**
-     * 私有文件字段
-     */
-    protected $privateFiles = [];
-
-
-    /**
-     * 文件前缀
-     */
-    protected $filePrefix = "";
-
-    
-    /**
-     * 处理输入文件类字段
-     */
-    protected function filesInput( & $model ) {
-
-        // 文件前缀
-        if ( strpos( $this->filePrefix, "@") === 0 ) {
-            $perfix = substr($this->filePrefix, 1, strlen($this->filePrefix));
-            $this->filePrefix = $model->$perfix;
-        }
-
-        $files = $this->getFiles();
-
-        foreach( $files as $attr=>$isPrivate ) {
-
-            // 单一文件路径
-            if ( is_string($model->$attr) ) {
-                $model->$attr = $this->writeFile( $model->$attr, $isPrivate );
-            
-            // 二维数组
-            } else {
-
-                // 批量处理文件
-                $values = $model->$attr;
-                foreach( $values as $key=>$path ) {
-                    $values[$key] = $this->writeFile( $path, $isPrivate );
-                }
-                $model->$attr = $values;
-            }
-        }
-    }
-
-    /**
-     * 读取文件字段清单
-     */
-    private function getFiles() {
-
-        $files = [];
-        if ( !empty($this->publicFiles) ) {
-            $files = array_merge(
-                $files,
-                array_combine( $this->publicFiles, array_fill(0, count($this->publicFiles), false) )
-            );
-        }
-
-        if ( !empty($this->privateFiles) ) {
-            $files = array_merge(
-                $files,
-                array_combine( $this->privateFiles, array_fill(0, count($this->privateFiles), true) )
-            );
-        }
-
-        return $files;
-    }
-
-
-    /**
-     * 保存文件
-     * @param $path 文件路径
-     * @param $private 是否为私有文件
-     * @return string 文件名称;
-     */
-    protected function writeFile( $path, $private=false ) {
-
-        if ( strpos( $path, "@") !== 0 ) {
-            return $path;
-        }
-
-        $path = substr($path, 1, strlen($path));
-        if ( !file_exists($path) ) {
-            throw Excp::create("文件不存在({$path})", 404);
-        }
-
-        // 转存到指定的存储引擎
-        $ext = pathinfo($path, PATHINFO_EXTENSION); 
-        $name = FS::getPathName( $ext, $this->filePrefix );
-        $stream = fopen($path, 'r');
-        FS::writeStream($name, $stream, [
-            'visibility' => $private ? AdapterInterface::VISIBILITY_PRIVATE : AdapterInterface::VISIBILITY_PUBLIC
-        ]);
-        if (is_resource($stream)) {
-            fclose($stream);
-        }
-
-        return $name;
-    }
-
-    /**
-     * 处理输入文件类字段读取
-     */
-    protected function filesOutput( & $model ) {
-    }
 
 
     /**
@@ -150,8 +23,8 @@ class Observer {
      * @param Model $user
      * @return void
      */
-    public function creating( $store ) {
-        $this->filesInput( $store );
+    public function creating( $model ) {
+        $model->filesInput();
     }
 
 }
