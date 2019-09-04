3a358224b46cd37a1c4e10c7fb8167b62768b031
Max <max@diancloud.com>
2019-09-04 15:13:56 +0800

    + 异步 Http Client

---
+++ yao/src/async/Http.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class Http
+ * 
+ * @package Yao
+ * @version $Revision$
+ * @author Max<https://github.com/trheyi>
+ * @copyright 2019 Vpin.biz
+ * @link https://www.vpin.biz
+ */
+
+namespace Yao\Async;
+use \Swlib\SaberGM;
+
+/**
+ * 异步 Http Client 
+ * see https://github.com/swlib/saber
+ */
+class Http extends SaberGM {
+
+}
